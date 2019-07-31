f52a1a910b42fb7e5b6713061de7598d23d143dd
Rawann Silva <rawannsilveira@gmail.com>
2019-07-30 21:41:34 -0300

    Projeto teste seção 125

---
+++ CourseSeção125/Entities/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseSeção125.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void WithDraw (double amount)
+        {
+            Balance -= amount;
+        } 
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ CourseSeção125/Program.cs
@@ -0,0 +1,15 @@
+﻿using CourseSeção125.Entities;
+using System;
+
+namespace CourseSeção125
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "rawann", 100.0, 500.0);
+            Console.WriteLine(account.Balance);
+
+        }
+    }
+}
